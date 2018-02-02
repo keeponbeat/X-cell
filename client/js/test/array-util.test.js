10000088b3015108df253fdcb98957974beadba5
Toru Murakami <mrkm.toru@gmail.com>
2018-02-03 00:25:13 +0900

    add function getLetterRange to array-util.js and add tests to array-util.test.js

--- client/js/test/array-util.test.js
+++ client/js/test/array-util.test.js
@@ -1,6 +1,7 @@
-const { getRange } = require('../array-util');
+const { getRange, getLetterRange } = require('../array-util');
 
 describe('array-util', () => {
+	
 	describe('getRange()', () => {
 		it('produces a valid range starting with 0', () => {
 			expect(getRange(0, 5)).toEqual([0, 1, 2, 3, 4, 5]);
@@ -15,4 +16,18 @@ describe('array-util', () => {
 			expect(getRange(-10, 2)).toEqual([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2]);
 		});
 	});
+
+	describe('getLetterRange()', () => {
+		it('produces a valid range starting with Q', () => {
+			expect(getLetterRange('Q', 1)).toEqual(['Q']);
+		});
+		it('produces a valid range starting with A', () => {
+			expect(getLetterRange('A', 5)).toEqual(['A', 'B', 'C', 'D', 'E']);
+		});
+		it('pproduces a valid range starting with C', () => {
+			expect(getLetterRange('C', 3)).toEqual(['C', 'D', 'E']);
+		});
+	});
+
 });
+
